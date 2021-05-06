6db93abd431f3bb998834a8a04c12df81f5c4f2b
alberto <alberto.vs@outlook.com>
2021-05-06 08:57:51 -0500

    06052021_v2.3

--- src/main/java/com/proyecto/person/business/impl/PersonBusinessImpl.java
+++ src/main/java/com/proyecto/person/business/impl/PersonBusinessImpl.java
@@ -2,10 +2,7 @@ package com.proyecto.person.business.impl;
 
 import java.net.URI;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 0960e85427dbd31ad804b96fcafc0765f981fdb4
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Service;
 
-<<<<<<< HEAD
 
 import com.proyecto.person.business.IPersonBusiness;
 import com.proyecto.person.exception.BadRequestException;
@@ -23,12 +19,6 @@ import com.proyecto.person.model.Person;
 import com.proyecto.person.repo.IPersonRepo;
 
 
-=======
-import com.proyecto.person.business.IPersonBusiness;
-import com.proyecto.person.model.Person;
-import com.proyecto.person.repo.IPersonRepo;
-
->>>>>>> 0960e85427dbd31ad804b96fcafc0765f981fdb4
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -40,7 +30,6 @@ public class PersonBusinessImpl implements IPersonBusiness{
 
 	@Override
 	public Mono<ResponseEntity<Person>> insert(Person person, final ServerHttpRequest req) {
-<<<<<<< HEAD
 		Mono<Person> MoPerson = repo.insert(person);
 		return MoPerson.map(p -> ResponseEntity.created(URI.create(req.getURI().toString().concat("/").concat(p.getId())))
 				.contentType(MediaType.APPLICATION_JSON)
@@ -56,21 +45,6 @@ public class PersonBusinessImpl implements IPersonBusiness{
 		
 		return MoPersonDB
 				.zipWith(MoPersonReq,(db, p) -> {
-=======
-		Mono<Person> Moperson = repo.insert(person);
-		return Moperson.map(p -> ResponseEntity.created(URI.create(req.getURI().toString().concat("/").concat(p.getId())))
-				.contentType(MediaType.APPLICATION_JSON)
-				.body(p));
-	}
-
-	@Override
-	public Mono<ResponseEntity<Person>> edit(Person person, String id) {
-		Mono<Person> MopersonReq = Mono.just(person);
-		Mono<Person> MopersonDB = repo.findById(id);
-		
-		return MopersonDB
-				.zipWith(MopersonReq,(db, p) -> {
->>>>>>> 0960e85427dbd31ad804b96fcafc0765f981fdb4
 					db.setId(id);
 					db.setName(p.getName());
 					db.setLastname(p.getLastname());
@@ -81,7 +55,6 @@ public class PersonBusinessImpl implements IPersonBusiness{
 
 					return db;
 				})
-<<<<<<< HEAD
 				.flatMap(p -> repo.save(p))
 				.map(pe -> ResponseEntity.ok()
 						.contentType(MediaType.APPLICATION_JSON)
@@ -116,40 +89,6 @@ public class PersonBusinessImpl implements IPersonBusiness{
 			return repo.deleteById(p.getId())
 					.then(Mono.just(new ResponseEntity<Void>(HttpStatus.NO_CONTENT)));
 		});
-=======
-				.flatMap(p -> repo.save(p))//p -> repo.save(p)
-				.map(pe -> ResponseEntity.ok()
-						.contentType(MediaType.APPLICATION_JSON)
-						.body(pe))
-				.defaultIfEmpty(new ResponseEntity<Person>(HttpStatus.NOT_FOUND));
-	}
-	
-	@Override
-	public Mono<ResponseEntity<Flux<Person>>> list() {
-		Flux<Person> Fxperson = repo.findAll();
-		return Mono.just(ResponseEntity.ok()
-				.contentType(MediaType.APPLICATION_JSON)
-				.body(Fxperson));
-	}
-	
-	@Override
-	public Mono<ResponseEntity<Person>> listById(String id) {
-		Mono<Person> Moperson = repo.findById(id);
-		return Moperson.map(p -> ResponseEntity.ok()
-				.contentType(MediaType.APPLICATION_JSON)
-				.body(p))
-				.defaultIfEmpty(ResponseEntity.notFound().build());
-	}
-
-	@Override
-	public Mono<ResponseEntity<Void>> delete(String id) {
-		Mono<Person> Moperson = repo.findById(id);
-		return Moperson.flatMap(p -> {
-			return repo.deleteById(p.getId())
-					.then(Mono.just(new ResponseEntity<Void>(HttpStatus.NO_CONTENT)));
-		})
-				.defaultIfEmpty(new ResponseEntity<Void>(HttpStatus.NOT_FOUND));
->>>>>>> 0960e85427dbd31ad804b96fcafc0765f981fdb4
 	}
 	
 }
